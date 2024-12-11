998869b87557364024141fe35f654d4b2f559516
codek <code_k@163.com>
2024-12-11 16:39:01 +0800

    修改

--- CoreCms.Net.Web.Admin/Controllers/Order/CoreCmsOrderController.cs
+++ CoreCms.Net.Web.Admin/Controllers/Order/CoreCmsOrderController.cs
@@ -1064,11 +1064,11 @@ namespace CoreCms.Net.Web.Admin.Controllers
                     detailsStartNumber++;
                 }
                 //合并单元格（第几行，到第几行，第几列，到第几列）
-                var marId = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42 };
-                foreach (var id in marId)
-                {
-                    sheet1.AddMergedRegion(new CellRangeAddress(nowNumber + 1, detailsStartNumber, id, id));
-                }
+                //var marId = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42 };
+                //foreach (var id in marId)
+                //{
+                //    sheet1.AddMergedRegion(new CellRangeAddress(nowNumber + 1, detailsStartNumber, id, id));
+                //}
 
             }
 
@@ -1469,11 +1469,11 @@ namespace CoreCms.Net.Web.Admin.Controllers
                     detailsStartNumber++;
                 }
                 //合并单元格（第几行，到第几行，第几列，到第几列）
-                var marId = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42 };
-                foreach (var id in marId)
-                {
-                    sheet1.AddMergedRegion(new CellRangeAddress(nowNumber + 1, detailsStartNumber, id, id));
-                }
+                //var marId = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42 };
+                //foreach (var id in marId)
+                //{
+                //    sheet1.AddMergedRegion(new CellRangeAddress(nowNumber + 1, detailsStartNumber, id, id));
+                //}
 
             }
 
